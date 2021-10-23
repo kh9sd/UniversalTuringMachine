0f852a55d91af5c5dde3036135e5c27773a42948
kh9sd <kevin9sd@gmail.com>
2021-10-23 15:27:11 -0400

    made it actually run the turing machine WHOOOOOOO

--- turing-runner.py
+++ turing-runner.py
@@ -1,4 +1,5 @@
 # main event loop for turing machine
+import time
 from mconfig import MConfig
 from tape import Tape
 
@@ -9,16 +10,31 @@ def display_mconfig_dict(m_dict):
 
 
 input_sd = "DADDCRDAA;DAADDRDAAA;DAAADDCCRDAAAA;DAAAADDRDA;"
-
 mconfig_dict = {}
 
-position = 0
-
-tape = Tape()
-
+# initialization, add mconfigs to dict
 for chunk in input_sd.split(";")[:-1]:  # ignore last element, will be blank
     mconfig = MConfig(chunk)
+    mconfig_dict[mconfig.name] = mconfig
 
-    mconfig_dict[mconfig.get_name()] = mconfig
+# display_mconfig_dict(mconfig_dict)
 
-display_mconfig_dict(mconfig_dict)
+current_mcon = mconfig_dict["q_3"]
+# print_com, move_com = current_mcon.operation
+# print(print_com)
+
+# print(print_com[1:])
+tape = Tape()
+while True:
+    if tape.square_check(current_mcon.symbol):
+        print_com, move_com = current_mcon.operation
+
+        tape.set_symbol(print_com[1:])
+
+        tape.move(move_com)
+
+        print(tape)
+        current_mcon = mconfig_dict[current_mcon.next]
+    else:
+        raise RuntimeError("Tape's symbol doesn't match mconfig's")
+    time.sleep(0.5)
