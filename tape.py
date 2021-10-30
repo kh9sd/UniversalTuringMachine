5cc7ec3ac51b6f41b198e7205f279b2f16ebe7b6
kh9sd <kevin9sd@gmail.com>
2021-10-30 15:21:24 -0400

    testign if removing turing runner is any good

--- tape.py
+++ tape.py
@@ -1,3 +1,7 @@
+import time
+from mconfig import MConfig
+
+
 class TapeSquare:
     """
     represents a single square on the tape of a Turing machine
@@ -154,3 +158,39 @@ class Tape:
                 f"[{self.current.data}]" +
                 "\033[0m" +
                 right_str)
+
+
+def display_mconfig_dict(m_dict):
+    for mcon in m_dict:
+        print("{}: {}".format(mcon, mconfig_dict[mcon]))
+
+
+input_sd = "DADDCRDAA;DAADDRDAAA;DAAADDCCRDAAAA;DAAAADDRDA;"
+mconfig_dict = {}
+
+# initialization, add mconfigs to dict
+for chunk in input_sd.split(";")[:-1]:  # ignore last element, will be blank
+    mconfig = MConfig(chunk)
+    mconfig_dict[mconfig.name] = mconfig
+
+# display_mconfig_dict(mconfig_dict)
+
+current_mcon = mconfig_dict["q_1"]
+# print_com, move_com = current_mcon.operation
+# print(print_com)
+
+# print(print_com[1:])
+tape = Tape()
+while True:
+    if tape.square_check(current_mcon.symbol):
+        print_com, move_com = current_mcon.operation
+        tape.set_symbol(print_com[1:])
+        tape.move(move_com)
+
+        print(f"Current m-config: {current_mcon.name}")
+        print(tape)
+
+        current_mcon = mconfig_dict[current_mcon.next]
+    else:
+        raise RuntimeError("Tape's symbol doesn't match mconfig's")
+    time.sleep(0.5)
